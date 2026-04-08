5f35bd7655b18c3d670e33550270ec1f71ded915
Satwik-Dubey <dubeysatwik508@gmail.com>
2026-04-08 10:32:11 +0530

    added constants like holidays

---
+++ src/constants/holidays.js
@@ -0,0 +1,9 @@
+export const NATIONAL_HOLIDAYS = {
+  '01-01': "New Year's Day",
+  '01-26': "Republic Day",
+  '05-01': "Labor Day",
+  '08-15': "Independence Day",
+  '10-02': "Gandhi Jayanti",
+  '10-31': "Halloween",
+  '12-25': "Christmas Day"
+};
